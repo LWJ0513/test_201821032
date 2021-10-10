c2be0e543318b137336cb7a53a7f62287ba47353
WonJeong <showers0513@gmail.com>
2021-10-11 08:29:44 +0900

    구글 로그인 구현

--- app/src/main/java/kr/ac/kku/cs/test_201821032/signIn/LoginActivity.kt
+++ app/src/main/java/kr/ac/kku/cs/test_201821032/signIn/LoginActivity.kt
@@ -3,52 +3,79 @@ package kr.ac.kku.cs.test_201821032.signIn
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginResult
-import com.facebook.login.widget.LoginButton
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import kr.ac.kku.cs.test_201821032.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kr.ac.kku.cs.test_201821032.MainActivity
+import kr.ac.kku.cs.test_201821032.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     //이메일과 password를 입력받아 firebase auth에다가 전달
+    private lateinit var binding: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var callbackManager: CallbackManager
     private lateinit var userDB: DatabaseReference
 
+
+    private var googleSignInClient: GoogleSignInClient? = null
+    private var RC_SIGN_IN = 9001
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         userDB = Firebase.database.reference.child("Users")
         auth = Firebase.auth    // = FirebaseAuth.getInstance()랑 똑같음
         callbackManager = CallbackManager.Factory.create()
 
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("762777289795-7e223bhg95lsbrao3qcnrss9mg6afgl0.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
+
         initEmailLoginButton()
+        initGoogleLoginButton()
         initFacebookLoginButton()
     }
 
     private fun initEmailLoginButton() {
-        val emailLoginButton: Button = findViewById<Button>(R.id.emailLoginButton)
-
         emailLoginButton.setOnClickListener {
             startActivity(Intent(this, EmailLoginActivity::class.java))
 
         }
     }
 
-    private fun initFacebookLoginButton() {
-        val facebookLoginButton = findViewById<LoginButton>(R.id.facebookLoginButton)
+    private fun initGoogleLoginButton() {
+        googleLoginButton.setOnClickListener {
+            signIn()
+        }
+    }
 
+    private fun initFacebookLoginButton() {
         facebookLoginButton.setPermissions("email", "public_profile")   //페북 계정에서 어떤 정보를 가져올건지
         facebookLoginButton.registerCallback(
             callbackManager,
@@ -88,5 +115,41 @@ class LoginActivity : AppCompatActivity() {
     }
 
 
+    private fun signIn() {
+        val signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)!!
+            // 구글API가 넘겨주는 값 받아옴
+
+            if (result.isSuccess) {
+                var accout = result.signInAccount
+                firebaseAuthWithGoogle(accout)
+                Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // 아이디, 비밀번호 맞을 때
+                    if (auth.currentUser != null) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }
+                } else {
+                    // 틀렸을 때 //todo
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
 }
