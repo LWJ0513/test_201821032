601dc8c370af2106c89a452061dba306d90a4b3c
WonJeong <showers0513@gmail.com>
2021-10-15 03:19:43 +0900

    1:1 개설시 취미 db 추가

---
+++ app/src/main/java/kr/ac/kku/cs/test_201821032/signIn/Hobbylist.kt
@@ -0,0 +1,23 @@
+package kr.ac.kku.cs.test_201821032.signIn
+
+class Hobbylist {
+    companion object {
+        const val SPORTS = "sports"
+        const val FASHION = "fashion"
+        const val FUND = "fund"
+        const val IT = "IT"
+        const val GAME = "game"
+        const val STUDY = "stduy"
+        const val READING = "reading"
+        const val TRAVEL = "travel"
+        const val ENTERTAINMENT = "entertainment"
+        const val PET = "pet"
+        const val FOOD = "beauty"
+        const val BEAUTY = "beauty"
+        const val ART = "art"
+        const val DIY = "diy"
+        const val COUNSELING = "counseling"
+        const val RIDE = "ride"
+    }
+
+}
